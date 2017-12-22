b3d1416cfd493058575bef1acf2fd5c54602e0d2
interval_ky <player_ky@126.com>
2017-12-22 13:58:07 +0800

    style: 暂时隐藏部分内容

--- src/views/about/Info.vue
+++ src/views/about/Info.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="itv-about-info">
     <img src="../../assets/about/pic-info-1.jpg" alt="pic-info-1">
-    <img src="../../assets/about/pic-info-2.png" alt="pic-info-2">
+    <!-- <img src="../../assets/about/pic-info-2.png" alt="pic-info-2"> -->
     <img src="../../assets/about/pic-info-3.jpg" alt="pic-info-3">
     <img src="../../assets/about/pic-info-4.jpg" alt="pic-info-4">
   </div>
