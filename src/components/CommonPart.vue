0e11b1c2f7b12d7a6f07b8ac8cd25fdf37012c3b
Pony <mycnj123@gmail.com>
2017-12-25 17:28:21 +0800

    feat:增加点击微信卡券直接购买的功能

--- src/components/CommonPart.vue
+++ src/components/CommonPart.vue
@@ -24,83 +24,94 @@
 </template>
 
 <script>
-  import ApiWx from '../api/wx';
-  import ApiBuy from '../api/buy';
+import ApiWx from "../api/wx";
+import ApiBuy from "../api/buy";
 
-  export default {
-    name: 'ServiceCommonPart',
-    props:{
-      productId:{
-        type:String,
-      }
+export default {
+  name: "ServiceCommonPart",
+  props: {
+    productId: {
+      type: String
+    }
+  },
+  created() {
+    this.loadProduct();
+  },
+  data() {
+    return {
+      soldOut: false
+    };
+  },
+  methods: {
+    loadProduct() {
+      ApiBuy.getProduct(this.productId).then(res => {
+        if (res.data.code === 0) {
+          return;
+        } else if (res.data.code === 1104) {
+          this.soldOut = true;
+        }
+      });
     },
-    created(){
-      this.loadProduct();
-    },
-    data() {
-      return {
-        soldOut: false
-      }
-    },
-    methods: {
-      loadProduct(){
-        ApiBuy.getProduct(this.productId).then(res=>{
-          if(res.data.code === 0){
-            return;
-          } else if(res.data.code === 1104){
-            this.soldOut = true;
-          }
-        })
-      },
 
-      buy() {
-        if(!this.$cookies.getRaw('_prs_wx_user')){
-          location.href = process.env.NODE_HOST + `extensions/wx/user/authorize/?state=${encodeURIComponent(location.href)}`;
-        }
-        if (this.$bus.user.mobile === '' || !this.$bus.user.mobile ) {
-          this.$router.push({ name: 'Bind'})
-        } else{
-          ApiWx.getCardList().then(res => {
-            let _data = res.data.data;
-            wx.chooseCard({
-              cardType: _data.cardType, // 卡券类型
-              timestamp: _data.timestamp, // 卡券签名时间戳
-              nonceStr: _data.nonceStr, // 卡券签名随机串
-              signType: _data.signType, // 签名方式，默认'SHA1'
-              cardSign: _data.cardSign, // 卡券签名
-              success: (res) => {
-                let _cardList = res.cardList; // 用户选中的卡券列表信息
-                this.$bus.encryptCode = JSON.parse(_cardList)[0].encrypt_code;
-                setTimeout(()=>{
-                  this.$bus.productId = this.productId;
-                  this.$router.push({name: 'Buy'});
-                },200)
-              },
-            });
-          })
-        }
+    buy() {
+      if (!this.$cookies.getRaw("_prs_wx_user")) {
+        location.href =
+          process.env.NODE_HOST +
+          `extensions/wx/user/authorize/?state=${encodeURIComponent(
+            location.href
+          )}`;
+      }
+      if (this.$bus.user.mobile === "" || !this.$bus.user.mobile) {
+        this.$router.push({ name: "Bind" });
+      }
+      if (this.$route.query.encrypt_code) {
+        this.$bus.encryptCode = this.$route.query.encrypt_code;
+        setTimeout(() => {
+          this.$bus.productId = this.productId;
+          this.$router.push({ name: "Buy" });
+        }, 200);
+      } else {
+        ApiWx.getCardList().then(res => {
+          let _data = res.data.data;
+          wx.chooseCard({
+            cardType: _data.cardType, // 卡券类型
+            timestamp: _data.timestamp, // 卡券签名时间戳
+            nonceStr: _data.nonceStr, // 卡券签名随机串
+            signType: _data.signType, // 签名方式，默认'SHA1'
+            cardSign: _data.cardSign, // 卡券签名
+            success: res => {
+              let _cardList = res.cardList; // 用户选中的卡券列表信息
+              this.$bus.encryptCode = JSON.parse(_cardList)[0].encrypt_code;
+              setTimeout(() => {
+                this.$bus.productId = this.productId;
+                this.$router.push({ name: "Buy" });
+              }, 200);
+            }
+          });
+        });
       }
     }
   }
+};
 </script>
 <style lang="scss">
-  .itv-services-common {
-    .section {
-      position: relative;
-      .itv-base-button {
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-      }
-      .btn-1 {
-        top: 1020px;
-      }
-      .btn-2 {
-        bottom: 510px;
-      }
+.itv-services-common {
+  .section {
+    position: relative;
+    .itv-base-button {
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
     }
-    .buy{
-      padding: 8px 80px;
+    .btn-1 {
+      top: 1020px;
+    }
+    .btn-2 {
+      bottom: 510px;
     }
   }
+  .buy {
+    padding: 8px 80px;
+  }
+}
 </style>
